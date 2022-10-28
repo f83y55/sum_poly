8092250d6b39bc913d80f2edfb42e8f5df420308
f83y55 <f83y55@gmail.com>
2022-10-28 21:48:34 +0200

    commit

---
+++ readme.md
@@ -0,0 +1,52 @@
+This python3 script outputs, given : 
+ - a symbol (like X) and 
+ - a or many polynomial(s) expression(s) P (like X**2+3x),
+ outputs :
+   the sum of P(k) with k integer from 0 to n formula(s).
+
+
+Technically :
+ - sympy is required
+ - the script produces a sum_poly.html file, 
+     with MathJax (LaTeX) formatting (web access required)
+
+
+Usage exemple :
+# Welcome to sum_poly !
+# Press enter to continue...
+# Choose a symbol (defaut : x) :
+x
+# Enter a sequence of polynomial expressions (symbol : x, separator : ; ) :
+x ; x**2 ; x**3 ; x**5-2*x**2+10           
+# SUM for x from 0 to n : 
+#   Factor : n*(n + 1)/2
+#   Expand : n**2/2 + n/2
+# SUM for x**2 from 0 to n : 
+#   Factor : n*(n + 1)*(2*n + 1)/6
+#   Expand : n**3/3 + n**2/2 + n/6
+# SUM for x**3 from 0 to n : 
+#   Factor : n**2*(n + 1)**2/4
+#   Expand : n**4/4 + n**3/2 + n**2/4
+# SUM for x**5 - 2*x**2 + 10 from 0 to n : 
+#   Factor : (n + 1)*(2*n**5 + 4*n**4 + n**3 - 9*n**2 - 4*n + 120)/12
+#   Expand : n**6/6 + n**5/2 + 5*n**4/12 - 2*n**3/3 - 13*n**2/12 + 29*n/3 + 10
+
+
+HTML file's source code :
+<!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset="utf-8"/> <title>sum_poly</title>
+            <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
+            <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+        </head>
+        <body>
+\(  \displaystyle\sum_{ x=0 }^{ n } x = \frac{n \left(n + 1\right)}{2} = \frac{n^{2}}{2} + \frac{n}{2} \) <br>
+\(  \displaystyle\sum_{ x=0 }^{ n } x^{2} = \frac{n \left(n + 1\right) \left(2 n + 1\right)}{6} = \frac{n^{3}}{3} + \frac{n^{2}}{2} + \frac{n}{6} \) <br>
+\(  \displaystyle\sum_{ x=0 }^{ n } x^{3} = \frac{n^{2} \left(n + 1\right)^{2}}{4} = \frac{n^{4}}{4} + \frac{n^{3}}{2} + \frac{n^{2}}{4} \) <br>
+\(  \displaystyle\sum_{ x=0 }^{ n } x^{5} - 2 x^{2} + 10 = \frac{\left(n + 1\right) \left(2 n^{5} + 4 n^{4} + n^{3} - 9 n^{2} - 4 n + 120\right)}{12} = \frac{n^{6}}{6} + \frac{n^{5}}{2} + \frac{5 n^{4}}{12} - \frac{2 n^{3}}{3} - \frac{13 n^{2}}{12} + \frac{29 n}{3} + 10 \)
+<!--insert--here-->
+        </body>
+        </html>
+
+
---
+++ sum_poly.py
@@ -0,0 +1,57 @@
+import sympy.core, sympy.parsing.sympy_parser
+
+
+def sum_poly(P, X) :
+    N, Q = sympy.degree(P)+1, 0
+    for i in range(N+1) :
+        L = sympy.prod([X-j for j in range(N+1) if j != i])
+        s = sum([P.subs(X, k) for k in range(i+1)])
+        Q += sympy.Rational((-1)**(N-i), sympy.factorial(i)*sympy.factorial(N-i)) * s * L
+    return sympy.factor(Q), sympy.expand(Q)
+
+
+
+def go() :
+    x = input("# Choose a symbol (defaut : x) :\n")
+    x, n = sympy.symbols(x) if x else sympy.symbols('x'), sympy.symbols('n')
+    html_str = html_add()
+    lsP = input(f"# Enter a sequence of polynomial expressions (symbol : {x}, separator : ; ) :\n")
+    lsP = [sympy.parsing.sympy_parser.parse_expr(P.strip()) if P.strip() else 0 for P in lsP.split(';')]
+    lsQ = [sum_poly(P, x) for P in lsP]
+    [print(f"# SUM for {P} from 0 to {n} : \n#   Factor : {Q[0].subs(x, n)}\n#   Expand : {Q[1].subs(x, n)}") for P, Q in zip(lsP, lsQ)]
+    html_str = html_add(html_str, " <br>\n".join(latex_convert(P, Q, x, n) for P, Q in zip(lsP, lsQ)))
+    with open("sum_poly.html", 'w') as f :
+        f.write(html_str)
+
+
+def latex_convert(P, Q, X, n) :
+    latex_str = f"\(  \displaystyle\sum_{{ {X}=0 }}^{{ {n} }} {sympy.latex(P)} = {sympy.latex(Q[0].subs(X,n))} = {sympy.latex(Q[1].subs(X,n))} \)"
+    return latex_str
+
+
+def html_add(html_str:str="", html_str_include:str=""):
+    html_str_default = """
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset="utf-8"/> <title>sum_poly</title>
+            <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
+            <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+        </head>
+        <body>
+        <!--insert--here-->
+        </body>
+        </html>
+        """
+    if not(html_str) :
+        html_str = html_str_default
+    if '<!--insert--here-->' in html_str :
+        html_str = html_str.replace('<!--insert--here-->', html_str_include+'\n<!--insert--here-->')
+    return html_str
+
+        
+
+if __name__ == "__main__":
+    while not(input("\n# Welcome to sum_poly !\n# Press enter to continue...")) :
+        go()
+
